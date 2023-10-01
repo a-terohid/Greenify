4a887133ba2ec15bf656dfe14e95c7f5ff916ec5
a-terohid <87315139+a-terohid@users.noreply.github.com>
2023-10-01 19:24:01 +0330

    import datas

---
+++ public/data/blogs.js
@@ -0,0 +1,24 @@
+import blogs1 from "@/public/img/blogs1.png"
+import blogs2 from "@/public/img/blogs2.png"
+import blogs3 from "@/public/img/blogs3.png"
+
+export const blogs = [
+    {
+        id: 1,
+        title: "Embracing Sustainability",
+        info: "The Power of Eco-Friendly Products at Greenify",
+        img: blogs1,
+    },
+    {
+        id: 2,
+        title: "Sustainable Living Made Simple",
+        info: "How Greenify Helps You Make a Difference",
+        img: blogs2,
+    },
+    {
+        id: 3,
+        title: "Greenify's Guide to Eco-Friendly Shopping",
+        info: "Reducing Your Carbon Footprint One Purchase at a Time",
+        img: blogs3,
+    },
+]
--- public/data/customers.js
+++ public/data/customers.js
@@ -1,9 +1,47 @@
+import jack from "@/public/img/jack.png"
+import sarah from "@/public/img/sarah.png"
+import emily from "@/public/img/emily.png"
+import mark from "@/public/img/mark.png"
+import Jonathon from "@/public/img/Jonathon.png"
+import Sophia from "@/public/img/Sophia.png"
+
 
 export const customers = [
     {
         id: 1,
-        name: "",
-        review: "",
-        img: ""
+        name: "Sarah Johnson",
+        review: "I absolutely love my Organic Cotton Tote Bag from Greenify! It's not only stylish but also eco-friendly. Knowing that I'm making a positive impact on the environment with my purchase makes me feel great. Highly recommend this sustainable accessory!",
+        img: sarah
     },
+    {
+        id: 2,
+        name: "Mark Anderson",
+        review: "The Bamboo Toothbrushes from Greenify are a game-changer! The quality is outstanding, and I love the fact that they are made from renewable bamboo. My oral care routine just got a lot greener, and I couldn't be happier. Kudos to Greenify for offering such fantastic eco-friendly products!",
+        img: mark
+    },
+    {
+        id: 3,
+        name: "Emily Lee",
+        review: "I recently bought the Hemp Backpack from Greenify, and I must say it's a fantastic  investment. The durability of the hemp material is impressive, and I feel good knowing I'm choosing a sustainable alternative. This backpack is perfect for my outdoor adventures and daily commutes. Thumbs up for the eco-conscious design!",
+        img: emily
+    },
+    {
+        id: 4,
+        name: "Jack Carter",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum finibus egestas. Donec ipsum eros, iaculis id mauris non, fringilla cursus lectus. Praesent vel iaculis nunc. Etiam sodales imperdiet lectus a placerat. Nunc dictum ex enim, non blandit velit tincidunt vitae. Donec nec ex nisl. Donec malesuada, dui sed sollicitudin.",
+        img: jack
+    },
+    {
+        id: 5,
+        name: "Jonathon Thomas",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum finibus egestas. Donec ipsum eros, iaculis id mauris non, fringilla cursus lectus. Praesent vel iaculis nunc. Etiam sodales imperdiet lectus a placerat. Nunc dictum ex enim, non blandit velit tincidunt vitae. Donec nec ex nisl. Donec malesuada, dui sed sollicitudin.",
+        img: Jonathon
+    },
+    {
+        id: 6,
+        name: "Sophia Hester",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum finibus egestas. Donec ipsum eros, iaculis id mauris non, fringilla cursus lectus. Praesent vel iaculis nunc. Etiam sodales imperdiet lectus a placerat. Nunc dictum ex enim, non blandit velit tincidunt vitae. Donec nec ex nisl. Donec malesuada, dui sed sollicitudin.",
+        img: Sophia
+    },
+
 ]
